0c10717e18574a8806efb5ea242cc7c498d51aa6
Arun Chaitanya jami <arun.chaitanya@ntsindia.co.in>
2022-07-14 00:13:28 +0530

    Update intigration test sample teat with docblock

--- tests/integration/SampleTest.php
+++ tests/integration/SampleTest.php
@@ -4,6 +4,11 @@ namespace AwesomeMotive\Tests\integration;
 use PHPUnit\Framework\TestCase;
 
 class SampleTest extends TestCase {
+
+	/**
+	 * @test
+	 * @return void
+	 */
 	public function it_should_perform_sample_assertions() {
 		$sample = 1;
 		$this->assertEquals( $sample, 1 );
